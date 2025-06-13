e2b82a2ef12707bac409074096170b542b12b7ed
NISARG ASLE <136323108+nisargasle3012@users.noreply.github.com>
2025-06-14 01:19:06 +0530

    Update Login.jsx

--- Frontend/src/components/Auth/Login.jsx
+++ Frontend/src/components/Auth/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
   setMessage('');
   setLoading(true);
   try {
-    const res = await axios.post('http://localhost:5000/api/login', formData);
+    const res = await axios.post('https://full-stack-music-player.onrender.com/api/signup', formData);
     setMessage(res.data.message);
 
     setUser(res.data.user); // Save user data
